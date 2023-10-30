c8a87be7c70ab1bf7b07090ac75e57bce2a11acc
Cory <cory.leclair@gmail.com>
2023-10-30 11:35:09 -0400

    initial commit

---
+++ env.sh
@@ -0,0 +1,8 @@
+export AZURE_SUBSCRIPTION='CF_subscription'
+export LOC='Canada Central'
+export MY_STORAGE_ACCOUNT='CF_mystorageaccount'
+export MY_RESOURCE_GROUP='CF_MyResourceGroup'
+export FUNCTION_APP_NAME='haibunFunctionPocOrchestrator'
+export TEST_HOST=http://localhost:9023
+export TEST_BATCHES=2
+export TEST_INSTANCES=10
---
+++ src/functions/haibunFunctionPoc.ts
@@ -0,0 +1,47 @@
+import { app, HttpHandler, HttpRequest, HttpResponse, InvocationContext } from '@azure/functions';
+import * as df from 'durable-functions';
+import { OrchestrationContext, OrchestrationHandler } from 'durable-functions';
+import { haibunPlaywrightActivity, TActionResult } from './haibunPlaywrightActivity.js';
+
+const activityName = 'haibunFunctionPoc';
+const host = process.env.TEST_HOST;
+const batches = parseInt(process.env.TEST_BATCHES);
+const instances = parseInt(process.env.TEST_INSTANCES);
+
+const haibunFunctionPocOrchestrator: OrchestrationHandler = function* (context: OrchestrationContext) {
+    let outputs: TActionResult[] = [];
+    let failures = [];
+    for (let batch = 0; batch < batches; batch++) {
+        const tasks = [];
+        for (let instance = 0; instance < instances; instance++) {
+            tasks.push(context.df.callActivity(activityName, { batch, instance, content: `using firefox browser\ngo to the "${host}/home.html?${batch}-${instance}" webpage\nClick on dest\nBe on the ${host}/dest.html?${batch}-${instance} webpage` }));
+        }
+        const results = yield context.df.Task.all(tasks);
+        failures.push(...results.filter(r => !r.ok).map(r => r.result.failure));
+        outputs = [...outputs, results];
+    }
+
+    const total = batches * instances;
+    const totalDuration = outputs.reduce((a, o) => a + o[0].duration, 0);
+    return [outputs, failures, { totalDuration, total, average: totalDuration / total }];
+};
+df.app.orchestration('haibunFunctionPocOrchestrator', haibunFunctionPocOrchestrator);
+
+df.app.activity(activityName, { handler: haibunPlaywrightActivity });
+
+const haibunFunctionPocHttpStart: HttpHandler = async (request: HttpRequest, context: InvocationContext): Promise<HttpResponse> => {
+    const client = df.getClient(context);
+    const body: unknown = await request.text();
+    const instanceId: string = await client.startNew(request.params.orchestratorName, { input: body });
+
+    context.log(`Started orchestration with ID = '${instanceId}'.`);
+
+    return client.createCheckStatusResponse(request, instanceId);
+};
+
+app.http('haibunFunctionPocHttpStart', {
+    route: 'orchestrators/{orchestratorName}',
+    extraInputs: [df.input.durableClient()],
+    handler: haibunFunctionPocHttpStart,
+});
+
